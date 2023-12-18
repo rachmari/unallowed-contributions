c125a2a1fb904a22d7b4b1e03da1ee3705f646e1
Rachael Sewell <rachmari@github.com>
2023-12-18 13:19:05 -0800

    Update check-content-type.js

--- src/workflows/check-content-type.js
+++ src/workflows/check-content-type.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import { readFileSync } from 'fs'
 import coreLib from '@actions/core'
-import matter from 'gray-matter'
+
+import { checkContentType } from '#src/workflows/fm-utils.js'
 
 const { FILE_PATHS_CONTENT_TYPES, CONTENT_TYPE } = process.env
 
@@ -10,21 +10,10 @@ main()
 
 async function main() {
   const filePaths = JSON.parse(FILE_PATHS_CONTENT_TYPES)
-  const containsRai = await checkContentType(filePaths, CONTENT_TYPE)
+  const containsRai = checkContentType(filePaths, CONTENT_TYPE)
   if (containsRai.length === 0) {
     coreLib.setOutput('contentType', false)
   } else {
     coreLib.setOutput('contentType', true)
   }
-}
-
-export async function checkContentType(filePaths, type) {
-  const unallowedChangedFiles = []
-  for (const filePath of filePaths) {
-    const { data } = matter(readFileSync(`./${filePath}`, 'utf8'))
-    if (data.type === type) {
-      unallowedChangedFiles.push(filePath)
-    }
-  }
-  return unallowedChangedFiles
 }
