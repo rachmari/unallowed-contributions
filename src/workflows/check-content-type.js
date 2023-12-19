d4d63d1904cac6e6264227af2fdef857e2b3e2c6
Rachael Sewell <rachmari@github.com>
2023-12-19 12:39:46 -0800

    Update check-content-type.js

--- src/workflows/check-content-type.js
+++ src/workflows/check-content-type.js
@@ -12,8 +12,10 @@ async function main() {
   const filePaths = JSON.parse(FILE_PATHS_CONTENT_TYPES)
   const containsRai = checkContentType(filePaths, CONTENT_TYPE)
   if (containsRai.length === 0) {
+    console.log(`No files with content type ${CONTENT_TYPE} found.`)
     coreLib.setOutput('contentType', false)
   } else {
+    console.log(`Files with content type ${CONTENT_TYPE} found!`)
     coreLib.setOutput('contentType', true)
   }
 }
